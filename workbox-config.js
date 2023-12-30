4093cc5cae2462921563b15daf8820402e0c5c03
corka149 <corka149@mailbox.org>
2023-12-30 08:32:04 +0100

    Add offline cap

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,9 @@
 module.exports = {
-	globDirectory: 'public/',
-	globPatterns: [
-		'**/*.{svg,css,js,ttf,ico,png,html,pdf}'
-	],
-	swDest: 'public/sw.js',
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	]
+  navigateFallback: "offline.html",
+  globDirectory: "public/",
+  globPatterns: ["**/*.{svg,css,js,ttf,ico,png,html,pdf}"],
+  swDest: "public/sw.js",
+  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+  maximumFileSizeToCacheInBytes: 2097152 * 10,
+  sourcemap: false,
 };
